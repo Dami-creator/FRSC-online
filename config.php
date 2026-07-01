3b3c4de38deeef032b2fb8b50f2dd40d2910da89
Dami-creator <mcallendiana@gmail.com>
2026-07-01 17:20:00 +0100

    Add Paystack configuration to config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define('PAYSTACK_SECRET', 'sk_live_5c95210b9f1a0ffba3356a2d85343c15e3ca2157');
+define('PAYSTACK_PUBLIC', 'pk_live_b5622bad3a685b2e2d781934f3f1e84146dae9fe');
+define('AMOUNT', 5000); // in kobo (₦5,000)
+define('CURRENCY', 'NGN');
+?>
