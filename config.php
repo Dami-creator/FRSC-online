ee7964f02cdbfa102448c05fc778feb41fb064e8
Dami-creator <mcallendiana@gmail.com>
2026-07-02 17:04:00 +0100

    Clarify AMOUNT constant comment in config.php
    Updated the comment for the AMOUNT constant to clarify that it is in kobo.

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 define('PAYSTACK_SECRET', 'sk_live_5c95210b9f1a0ffba3356a2d85343c15e3ca2157');
 define('PAYSTACK_PUBLIC', 'pk_live_b5622bad3a685b2e2d781934f3f1e84146dae9fe');
-define('AMOUNT', 500000); // ₦5,000
+define('AMOUNT', 500000); // ₦5,000 in kobo
 define('CURRENCY', 'NGN');
 ?>
